83adddb0f6b2e0582f9504a69fa69a2fa25c9388
its-leofisher <lcfisher15@outlook.com>
2024-09-08 18:20:28 -0700

    #2 - create setup guide
    Import environment variables
    
    merge discover devices functions from setup into one, it determines if its triggered by setup process or being called from the app to ensure the device is active
    
    Moved set primary device json file and cached device json file to own functions for reusability

--- utils.py
+++ utils.py
@@ -1,13 +1,21 @@
 import time
 import json
 import os
+import sys
+import asyncio
 from kasa import Discover, SmartBulb
 import logging
+from dotenv import load_dotenv
 
+load_dotenv()
 logger = logging.getLogger(__name__)
 
-CACHE_FILE = 'device_cache.json'
-CACHE_EXPIRY = 12 * 60 * 60  # 12 hours
+if (int(os.getenv('ENABLE_APP_DEBUG'))):
+    logging.basicConfig(level=logging.DEBUG)
+
+CACHE_FILE = os.getenv('CACHE_FILENAME')
+CACHE_EXPIRY = int(os.getenv('CACHE_EXPIRY_SECONDS'))
+PRIMARY_DEVICE_FILE = os.getenv('PRIMARY_DEVICE_FILENAME')
 
 def rgb_to_hsv(r, g, b):
     """Convert RGB to HSV."""
@@ -30,20 +38,117 @@ def rgb_to_hsv(r, g, b):
     v = mx
     return int(h), int(s * 100), int(v * 100)
 
-async def discover_and_cache_device(target_alias):
-    devices = await Discover.discover()
-    for addr, dev in devices.items():
+async def discover_and_cache_devices(target_alias='',is_setup=False):
+    """Attempt to discover devices on network and store target device alias
+    alias to Cache and Storage for future retrieval"""
+    task = asyncio.create_task(loading_dots('Searching for devices'))
+
+    try:
+        devices = await Discover.discover()
+
+        if not devices:
+            print('\n\nDevice not found❗ Make sure to setup your device and connect it to your network, then try again.')
+            await cancel_task(task)
+            return False
+
+    except Exception as e:
+        print(f'\nCritical error occurred: {e}')
+
+    # ensure async task for dot loading is cancelled properly
+    await cancel_task(task)
+
+    devices_list = []
+
+    item_number = 0
+
+    for ip_addr, dev in devices.items():
+
         await dev.update()
-        if isinstance(dev, SmartBulb) and dev.alias == target_alias:
-            # Cache the device IP
-            cache_data = {
-                'ip': addr,
-                'timestamp': time.time()
-            }
-            with open(CACHE_FILE, 'w') as f:
-                json.dump(cache_data, f)
-            return addr
-    return None
+
+        if isinstance(dev, SmartBulb) and dev.alias:
+
+           # If target alias passed in then select it automatically otherwise perform prompt
+           if dev.alias == target_alias:
+                set_primary_device_data(dev.alias, ip_addr)
+                set_cached_device(ip_addr)
+                return ip_addr
+           else:
+                if not is_setup:
+                    print("\nLooks like setup.py was not run, triggering setup process.")
+                print("\nList of Devices")
+                print("=====================")
+                print(f"{item_number}: {dev.alias} @ {ip_addr}")
+                devices_list.append([dev.alias, ip_addr])
+        else:
+           print('\nDevice is not compatible.')
+
+        ++item_number
+
+    if not target_alias and is_setup:
+        if pick_device(devices_list):
+            return True
+
+def pick_device(devices_list):
+    """Prompt the user to pick a device."""
+
+    while True:
+        print('\nProvide the number of the device you want to respond to events...')
+        device_picked = input("> Enter number: ")
+
+        try:
+            device_picked = int(device_picked)
+            if not devices_list[device_picked]:
+                raise IndexError()
+            break
+        except ValueError:
+            print("Invalid input. Please enter a valid integer.")
+        except IndexError:
+            print("That number does not correspond to a device number on the list. Try again.")
+        except KeyboardInterrupt:
+            print("\nKeyboard interrupt received. Exiting discover devices set up.")
+            sys.exit(0)
+        except Exception as e:
+            print(f"Error: {e}")
+
+    device_alias = devices_list[device_picked][0]
+
+    ip_addr = devices_list[device_picked][1]
+
+    set_primary_device_data(device_alias, ip_addr)
+
+    set_cached_device(ip_addr)
+
+    print(f"\nThank you! Now storing device alias and ip to {PRIMARY_DEVICE_FILE}.")
+
+    print("Continue to Step 3 in README.md")
+
+    print("Note: If you want to change the device number in the future re-run setup.")
+
+    return True
+
+def set_primary_device_data(device_alias, ip_addr):
+    """Set primary device, used in app.py to know which device to communicate with"""
+    store_device_data = {
+        'alias': device_alias,
+        'ip': ip_addr
+    }
+
+    with open(PRIMARY_DEVICE_FILE, 'w') as f:
+        json.dump(store_device_data, f)
+
+    return True
+
+def set_cached_device(ip_addr):
+    """Device data is cached to not always run discovery process"""
+    cache_data = {
+        'ip': ip_addr,
+        'timestamp': time.time()
+    }
+
+    with open(CACHE_FILE, 'w') as f:
+        json.dump(cache_data, f)
+
+    return True
 
 def load_cached_device():
     if os.path.exists(CACHE_FILE):
@@ -57,3 +162,38 @@ def load_cached_device():
             return None
     return None
 
+def retrieve_primary_device():
+    if os.path.exists(PRIMARY_DEVICE_FILE):
+        try:
+            with open(PRIMARY_DEVICE_FILE, 'r') as f:
+                data = json.load(f)
+                return data['alias']
+        except (json.JSONDecodeError, KeyError) as e:
+            logger.error(f"Error reading file: {e}")
+            return None
+    return None
+
+async def cancel_task(task):
+    task.cancel()
+
+    try:
+        await task
+    except asyncio.CancelledError:
+        pass
+
+async def loading_dots(text='Loading'):
+    while True:
+        for dot in ['.', '..', '...']:
+            print(f'\r{text}{dot}', end='')
+            sys.stdout.flush()
+            await asyncio.sleep(0.5)
+
+def create_file(file_name):
+    if not os.path.exists(file_name):
+        with open(file_name, 'w') as file:
+            file.write('')
+        print(f'File {file_name} created successfully.')
+        return True
+    else:
+        print(f'File {file_name} already exists.')
+        return False
